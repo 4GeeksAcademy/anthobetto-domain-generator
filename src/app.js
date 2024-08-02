a812d6946b41e73019f8c25b64228511790e76ce
Anthony Flores <64795591+Anthobetto@users.noreply.github.com>
2024-08-02 13:04:23 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let newDomain = '';
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        newDomain += '<li class="list-group-item">' + pronoun[i] + adj[j] + noun[k] + '.com' + '</li>';
+      }
+    }
+  }
+  document.querySelector(".list-group").innerHTML = newDomain;
+}
+
